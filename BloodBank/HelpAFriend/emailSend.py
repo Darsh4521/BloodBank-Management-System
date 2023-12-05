5ea628c6675bd778eab1ecd2dc892e0500faea4c
Darsh4521 <66157646+Darsh4521@users.noreply.github.com>
2023-12-05 01:10:53 -0500

    Update emailSend.py

--- BloodBank/HelpAFriend/emailSend.py
+++ BloodBank/HelpAFriend/emailSend.py
@@ -4,5 +4,5 @@ content = "Hello "
 mail = smtplib.SMTP('smtp.gmail.com',587)
 mail.ehlo()
 mail.starttls()
-mail.login('helpafriend.bloodbank@gmail.com','bloodbank.project')
+mail.login('email@example.com','enter.password')
 mail.sendmail('helpafriend.bloodbank@gmail.com','divyashah.qa@gmail.com',content)
